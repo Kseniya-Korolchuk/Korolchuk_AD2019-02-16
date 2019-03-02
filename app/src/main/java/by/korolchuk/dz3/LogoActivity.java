f91e405124fc55c2ec21d9693a5b89aa0c8b4e77
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2019-03-02 12:20:49 +0300

    Login_screen

---
+++ app/src/main/java/by/korolchuk/dz3/LogoActivity.java
@@ -0,0 +1,16 @@
+package by.korolchuk.dz3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import by.korolchuk.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}

678cba8e0aeaf6c707cdef51b4b95fbeb369127d
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2019-03-15 21:05:21 +0300

    progress bar / picasso-transformations fixed libraries added (?)

--- app/src/main/java/by/korolchuk/dz3/ImageDownloader.java
+++ app/src/main/java/by/korolchuk/dz3/ImageDownloader.java
@@ -3,9 +3,12 @@ package by.korolchuk.dz3;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewParent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.squareup.picasso.Picasso;
 
@@ -15,6 +18,7 @@ import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 public class ImageDownloader extends Activity implements View.OnClickListener {
 
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +38,18 @@ public class ImageDownloader extends Activity implements View.OnClickListener {
 
         EditText editText = findViewById(R.id.downloader_edit_text);
         String path = editText.getText().toString();
-        ImageView imageView = findViewById(R.id.downloader_image_view);
+        imageView = findViewById(R.id.downloader_image_view);
 
         Picasso
                 .get()
                 .load(path)
                 .placeholder(R.drawable.progress_animation)
                 .error(R.drawable.error_android)
-
                 .transform(new CropCircleTransformation())
                 .into(imageView, new com.squareup.picasso.Callback() {
                     @Override
                     public void onSuccess() {
-
+                        imageView.getLayoutParams().width = imageView.getWidth() * 5;
                     }
 
                     @Override
